d3cab786fd0f79e1ba93c6539683da9c4b4e8be5
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-20 19:44:19 +0700

    Create 4.1 Lesson - Floats.py

---
+++ Python3/Basic Concepts/4.1 Lesson - Floats.py
@@ -0,0 +1,7 @@
+# Which of these will no be stored as a float?
+# 7 is not stored in float but stored in integer
+
+# Fill in th blank with the output of this code
+print(1 + 2 + 3 + 4.0 + 5)
+
+# Output = 15.0
