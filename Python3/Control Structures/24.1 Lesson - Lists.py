1a5cc1545349216f1be7dbf8fa120627cb217d30
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 14:54:15 +0700

    Create 24.1 Lesson - Lists.py

---
+++ Python3/Control Structures/24.1 Lesson - Lists.py
@@ -0,0 +1,19 @@
+# What is the result of this code?
+nums = [5, 4, 3, 2, 1]
+print(nums[1])
+# result = 4
+
+# How many item are in list?
+# [2,]
+# answer = 1
+
+# Fill in the blanks to create a list and print its 3rd element.
+list = [42, 55, 67]
+print(list[2])
+
+# Which line of this code will cause an error?
+num = [5, 4, 3, [2], 1]
+print(num[0])
+print(num[3][0])
+print(num[5])
+# answer = line 4
