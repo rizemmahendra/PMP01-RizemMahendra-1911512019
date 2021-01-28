c44b318587054f218881da0e1e5e06439c881adf
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 13:37:53 +0700

    Create 14.1 Lesson - Taking User Input.py

---
+++ Python3/Strings & Variabel/14.1 Lesson - Taking User Input.py
@@ -0,0 +1,6 @@
+# Fill in the blank to take user input, assign it to a variable, and output it:
+text = input()
+print(text)
+
+# Fill in the blanks to prompt the user with a message and assign to input to the variable "name":
+name = input("What is your name?")
