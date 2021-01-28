196b3fb5214296f9bf9c1bbf35b53bad588b7be2
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 16:16:32 +0700

    Create 25.1 Lesson - List Operations.py

---
+++ Python3/Control Structures/25.1 Lesson - List Operations.py
@@ -0,0 +1,24 @@
+# What is the result of this code?
+nums = [1, 2, 3, 4, 5]
+nums[3] = nums[1]
+print(nums[3])
+# result = 2
+
+# Fill in the blanks to create a list, reassign its 2nd element and print the whole list.
+nums = [33, 42, 56]
+nums = [1]
+print(nums)
+
+# What is the result of this code?
+nums = [10, 9, 8, 7, 6, 5]
+nums[0] = nums[1] - 5
+if 4 in nums:
+  print(nums[3])
+else:
+  print(nums[4])
+# result = 7
+
+# Fill in the blanks to print "Yes" if the list contains 'z':
+letters = ['a', 'b', 'z']
+if "z" in letters:
+  print("Yes")
