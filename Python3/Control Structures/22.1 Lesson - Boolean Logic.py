4b57df8780977b1bf709bfe8e674dc563266d827
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 14:40:50 +0700

    Create 22.1 Lesson - Boolean Logic.py

---
+++ Python3/Control Structures/22.1 Lesson - Boolean Logic.py
@@ -0,0 +1,21 @@
+# What is the result of this code?
+if (1 == 1) and (2 + 2 > 3):
+  print("true")
+else:
+  print("false")
+# result = true
+
+# Fill in the blanks to print "Welcome".
+age = 15
+money = 500
+if age > 18 or money > 100:
+  print("Welcome")
+
+# What is the result of this code?
+if not True:
+  print("1")
+elif not(1 + 1 == 3):
+  print("2")
+else:
+  print("3")
+# result = 2
