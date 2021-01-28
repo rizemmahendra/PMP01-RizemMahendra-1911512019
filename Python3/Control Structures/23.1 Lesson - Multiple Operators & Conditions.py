9c9ae9beea1d80bd0ebc836bd2e8ef36c2601ebd
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 14:45:52 +0700

    Create 23.1 Lesson - Multiple Operators & Conditions.py

---
+++ Python3/Control Structures/23.1 Lesson - Multiple Operators & Conditions.py
@@ -0,0 +1,15 @@
+# What is the result of this code?
+if 1 + 1 * 3 == 6:
+  print("Yes")
+else:
+  print("No")
+# result = No
+
+# What is the result of this code?
+x = 4
+y = 2
+if not 1 + 1 == y or x == 4 and 7 == 8:
+  print("Yes")
+elif x > y:
+  print("No")
+# result = No
