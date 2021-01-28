243871dceaf1b1b4a6c7ae77dde07f3ccaba03ff
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 13:42:58 +0700

    Create 15.1 Lesson - Working with Input.py

---
+++ Python3/Strings & Variabel/15.1 Lesson - Working with Input.py
@@ -0,0 +1,11 @@
+# What will be the output f this code?
+x = "2"
+y = "4"
+z = int(x) + int(y)
+print(z)
+# output = 6
+
+# Fill in the blanks to produece a valid code, which takes the values of the x and y variables from the input and outputs their sum.
+x = int(input())
+y = int(input())
+print(x + y)
