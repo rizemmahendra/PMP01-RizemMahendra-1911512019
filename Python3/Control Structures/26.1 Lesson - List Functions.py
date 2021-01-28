4f96e3e753da9e2db2a25aedddca103ac914c45e
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 16:25:26 +0700

    Create 26.1 Lesson - List Functions.py

---
+++ Python3/Control Structures/26.1 Lesson - List Functions.py
@@ -0,0 +1,22 @@
+# What is the result of this code?
+words = ["hello"]
+words.append("world")
+print(words[1])
+# result = wolrd
+
+# What is the result of this code?
+letters = ["a", "b", "c"]
+letters.append("d")
+print(len(letters))
+# result = 4
+
+# What is the result of this code?
+nums = [9, 8, 7, 6, 5]
+nums.append(4)
+nums.insert(2, 11)
+print(len(nums))
+# result = 7
+
+# Drag and drop from the options below to add 'z' to the end of the list and print the list's length.
+list.append('z')
+print(len(list))
