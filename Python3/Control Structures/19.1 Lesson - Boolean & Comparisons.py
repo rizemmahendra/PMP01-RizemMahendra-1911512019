4d4bbc79fc0f31e00440b1d5f3b7ec0499c0721a
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 14:20:28 +0700

    Create 19.1 Lesson - Boolean & Comparisons.py

---
+++ Python3/Control Structures/19.1 Lesson - Boolean & Comparisons.py
@@ -0,0 +1,14 @@
+# What are the two Boolean values in Python?
+# answer = True and False
+
+# What is the output of this code?
+print(7 != 8)
+# output = True
+
+# What is the output of this code?
+print(7 > 7.0)
+# output = False
+
+# What is the output of this code?
+print(8.7 <= 8.70)
+# output = True
