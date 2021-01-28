51f7550c1c2af3f0593a1f423c9d2e38ed75f6ec
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 13:21:26 +0700

    Create 10.1 Lesson - Newlines.py

---
+++ Python3/Strings & Variabel/10.1 Lesson - Newlines.py
@@ -0,0 +1,9 @@
+# Which of the following options results in exactly two line?
+# answer = "Hello \n Wolrd"
+
+# How many lines will the following code output?
+print("hi")
+print("""This
+is
+great""")
+# answer = 4 Lines
