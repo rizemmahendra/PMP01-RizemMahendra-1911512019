261e4f69fd778e9c0b4022dbaeecc2a8fe5b7f60
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 13:59:45 +0700

    Create 17.1 Module 2 Quiz.py

---
+++ Python3/Strings & Variabel/17.1 Module 2 Quiz.py
@@ -0,0 +1,33 @@
+# What is the output of this code?
+spam = "7"
+spam = spam + "0"
+eggs = int(spam) + 3
+print(float(eggs))
+# output = 70.3
+
+# What is the output of this code if the user enter '42' as input
+age = int(input())
+print(age + 8)
+# output = 50
+
+# What is the output of this code?
+x = 5
+y = x + 3
+y = int(str(y) + "2")
+print(y)
+# output = 82
+
+# Fill in the blanks to declarate a variable, add 5 to it and print its value.
+x = 4
+x += 5
+print(x)
+
+# What is the output of this code?
+x = 3 
+num = 17
+print(num % x)
+# output = 2
+
+# Fill in the blanks to take a name as input and output a Welcome message, containing the input?
+name = = input()
+print("Welcome, " + name)
