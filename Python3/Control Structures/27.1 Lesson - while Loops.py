58e3356b01babf22f29e3419e6d1ec563119a62c
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 16:33:43 +0700

    Create 27.1 Lesson - while Loops.py

---
+++ Python3/Control Structures/27.1 Lesson - while Loops.py
@@ -0,0 +1,25 @@
+# How many numbers does this code print?
+i = 3
+while i >= 0:
+   print(i)
+   i = i - 1
+# answer = 4
+
+# Fill in the blanks to create a loop that increments the value of x by 2 and prints the even values.
+x = 0
+while x <= 20:
+	print(x)
+	x += 2
+
+# How many numbers does this code print?
+
+i = 5
+while True:
+  print(i)
+  i = i - 1
+  if i <= 2:
+    break
+# answer = 3
+
+Which statement ends the current iteration and continues with the next one?
+# answer = continue
