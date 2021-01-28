db8b443069a2523eef7281372f0fc4aa97a390f7
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 16:52:08 +0700

    Create 29.1 Lesson - Range.py

---
+++ Python3/Control Structures/29.1 Lesson - Range.py
@@ -0,0 +1,18 @@
+# what is the result of this code?
+nums = list(range(5))
+print(nums[4])
+# result = 4
+
+# What is the result of this code?
+nums = list(range(5, 8))
+print(len(nums))
+# result = 3
+
+# What is the result of this code?
+nums = list(range(3, 15, 3))
+print(nums[2])
+# result = 9
+
+# Fill in the blanks to create a for loop that prints only the even values in the range:
+for i in range(0, 20, 2):
+	print(i)
