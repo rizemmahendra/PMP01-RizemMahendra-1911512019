65c3dc96789fcb6a0374cf6cad18458be583cae1
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 16:41:30 +0700

    Create 28.1 Lesson - for Loops.py

---
+++ Python3/Control Structures/28.1 Lesson - for Loops.py
@@ -0,0 +1,15 @@
+# Fill in the blanks to create a valid for loop.
+letters = ['a', 'b', 'c']
+for l in letters:
+	print(l)
+  
+# What is the output of this code?
+list = [2, 3, 4, 5, 6, 7]
+for x in list:
+    if(x%2==1 and x>4):
+        print(x)
+        break
+ # output = 5
+ 
+# Which construct can be used to iterate through a list?
+# answer = Loops
