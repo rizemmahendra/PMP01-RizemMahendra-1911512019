9755df0d3c31e5b929d8435733031871ac06bafd
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 14:27:52 +0700

    Create 20.1 Lesson - if Statements.py

---
+++ Python3/Control Structures/20.1 Lesson - if Statements.py
@@ -0,0 +1,20 @@
+# What part of an if statement should be indented?
+# answer = The statement whitin it
+
+# What is the output of this code?
+spam = 7
+if spam > 5:
+  print("five")
+if spam > 8:
+  print("eight")
+# output = five
+
+# What is the output of this code?
+num = 7
+if num > 3:
+  print("3")
+  if num < 5:
+    print("5")
+    if num == 7:
+      print("7")
+# output = 3
