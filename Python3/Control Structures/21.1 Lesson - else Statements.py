fe76f80426f6cee8336e59e961d332e09e81fcce
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-28 14:34:05 +0700

    Create 21.1 Lesson - else Statements.py

---
+++ Python3/Control Structures/21.1 Lesson - else Statements.py
@@ -0,0 +1,18 @@
+# What is the result of this code?
+if 1 + 1 == 2:
+  if 2 * 2 == 8:
+    print("if")
+  else:
+    print("else")
+# result = else
+
+# Fill in the blanks to compare the variables and output the coresponding text:
+x = 10
+y = 20
+if x > y:
+  print("if statement")
+else:
+  print("else statement")
+  
+# A shorter option for an "else if" statement is:
+# answer = elif
