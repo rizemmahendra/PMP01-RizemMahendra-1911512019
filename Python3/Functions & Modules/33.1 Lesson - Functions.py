a95ea0979d1540c439444e84be60fc5a6dcdffcf
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-29 18:39:44 +0700

    Create 33.1 Lesson - Functions.py

---
+++ Python3/Functions & Modules/33.1 Lesson - Functions.py
@@ -0,0 +1,9 @@
+# Fill in the blanks to define a function named hello:
+def hello():
+	print("hi!")
+
+# Rearrange the code to declare a function sayHi() and call it.
+def sayHi():
+	print("Hi!")
+	
+sayHi()
