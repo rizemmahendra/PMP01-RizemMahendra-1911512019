0f9a27ef4e08578d75ee7a5385ac3b25a9d88892
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-29 19:04:56 +0700

    Create 35.1 Lesson - Returning from Functions.py

---
+++ Python3/Functions & Modules/35.1 Lesson - Returning from Functions.py
@@ -0,0 +1,15 @@
+# Fill in the blanks to define a function that compare  the lenghts of its arguments and returns the shortest one.
+def shoertest_string(x, y):
+  if len(x) <= len(y):
+    return x
+  else:
+    return y
+
+# What is the highest number this function prints if called?
+def print_numbers():
+  print(1)
+  print(2)
+  return
+  print(4)
+  print(6)
+# answer = 2
