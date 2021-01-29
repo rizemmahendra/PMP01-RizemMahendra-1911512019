8212f6d6c2c3415c025313a760ce9e67b0046f55
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-29 19:22:10 +0700

    Create 39.1 Lesson - The Standard Library & pip.py

---
+++ Python3/Functions & Modules/39.1 Lesson - The Standard Library & pip.py
@@ -0,0 +1,8 @@
+# Fill in the blanks to import the math module.
+import math
+
+# What name is given to Python's preinstalled modules?
+# answer = The Standard Library
+
+# What does PyPI stand for?
+# answer = Python Package Index
