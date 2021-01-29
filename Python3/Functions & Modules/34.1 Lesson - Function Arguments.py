b01960c856539d0a65fb6a104fc175765c9b78f3
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-29 18:44:07 +0700

    Create 34.1 Lesson - Function Arguments.py

---
+++ Python3/Functions & Modules/34.1 Lesson - Function Arguments.py
@@ -0,0 +1,17 @@
+# What is the result of this code?
+def print_double(x):
+   print(2 * x)
+
+print_double(3)
+# output = 6
+
+# Fill in the blanks to define a function that takes two arguments and prints their multiplication.
+def print_mult(x, y):
+	print(x * y)
+
+# Fill in the blanks to define a function that prints "Yes", if its parameter is an even number, and "No" otherwise.
+def even(x):
+	if x % 2 == 0:
+		print("Yes")
+	else:
+		print("No")
