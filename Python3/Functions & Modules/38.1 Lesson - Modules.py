9a24f7c2fc1d91637c47e30656f2483bd29bc1b2
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-29 19:18:42 +0700

    Create 38.1 Lesson - Modules.py

---
+++ Python3/Functions & Modules/38.1 Lesson - Modules.py
@@ -0,0 +1,16 @@
+# Which module is being used in this code?
+import math
+num = 10
+print (math.sqrt(num))
+# module being used is math module
+
+# Fill in the blanks to import only the sqrt and cos functions from the math module:
+from math import sqrt, cos
+
+# What error is caused by importing an unknown module?
+# answer = ImportError
+
+# What is the output of this code?
+import math as m
+print(math.sqrt(25))
+# output = An error occurs
