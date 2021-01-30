5595aff4811d3de6f6012c368f7dba73770d8526
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-30 11:48:08 +0700

    Create 47.1 Lesson - Opening Files.py

---
+++ Python3/Exceptions & Files/47.1 Lesson - Opening Files.py
@@ -0,0 +1,8 @@
+# Which function is used to access files?
+# answer = open
+
+# Drag and drop from the options below to open a file called "test.bin" in binary read mode.
+file = open("test.bin", "rb")
+
+# How would you close a file stored in a variable "text_file"?
+text_file.close()
