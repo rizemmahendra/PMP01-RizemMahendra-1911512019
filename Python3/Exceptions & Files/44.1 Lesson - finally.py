d13fb315cbb71e7b2e6440cdd55d686f55281132
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-30 11:20:00 +0700

    Create 44.1 Lesson - finally.py

---
+++ Python3/Exceptions & Files/44.1 Lesson - finally.py
@@ -0,0 +1,16 @@
+# What is the output of this code?
+try:
+  print(1)
+except:
+  print(2)
+finally:
+  print(3)
+# output = 13
+
+# Drag and drop from the options below to create a try/except/finally block.
+try:
+	print(1)
+except:
+	print(2)
+finally:
+	print(42)
