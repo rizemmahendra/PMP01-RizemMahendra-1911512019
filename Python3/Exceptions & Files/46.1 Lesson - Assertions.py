a03a2b4ca92c4f628893c4df769b3eb4ad57feab
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-30 11:42:14 +0700

    Create 46.1 Lesson - Assertions.py

---
+++ Python3/Exceptions & Files/46.1 Lesson - Assertions.py
@@ -0,0 +1,14 @@
+# What is the highest number printed by this code?
+print(0)
+assert "h" != "w"
+print (1)
+assert False
+print(2)
+assert True
+print(3)
+# highest number  printed is 1
+
+# Fill in the blanks to define a function that takes one argument. Assert the argument to be positive.
+def my_func(x):
+	assert x > 0, "Error!"
+	print(x)
