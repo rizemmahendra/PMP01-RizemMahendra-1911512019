0c0f1bf8bf2a409b6f62f1631a0a140869774b47
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-30 12:00:22 +0700

    Create 49.1 Lesson - Writing Files.py

---
+++ Python3/Exceptions & Files/49.1 Lesson - Writing Files.py
@@ -0,0 +1,8 @@
+# Which line of code writes "Hello world!" to a file?
+file.write("Hello world!")
+
+# What happens if you open a file in write mode and then immediately close it?
+# answer = The file contents are deleted
+
+# If a file write operation is successful, which one of these statements will be true?
+file.write(msg) == len(msg)
