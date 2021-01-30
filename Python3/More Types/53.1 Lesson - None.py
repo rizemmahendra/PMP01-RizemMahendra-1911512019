275cc273f1a15ac0567a8d211393db70f7e43df1
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-30 12:37:04 +0700

    Create 53.1 Lesson - None.py

---
+++ Python3/More Types/53.1 Lesson - None.py
@@ -0,0 +1,10 @@
+# What is "None" often used to represent?
+# answer = Absence of a value
+
+# What number does this code print?
+foo = print()
+if foo == None:
+   print(1)
+else:
+   print(2)
+# number will be print = 1
