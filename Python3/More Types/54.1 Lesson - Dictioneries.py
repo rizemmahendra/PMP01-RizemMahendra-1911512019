68a9f883315d58f4f9489f82a589c438526a1c1e
Rizem Mahendra <45225902+rizemmahendra@users.noreply.github.com>
2021-01-30 12:44:00 +0700

    Create 54.1 Lesson - Dictioneries.py

---
+++ Python3/More Types/54.1 Lesson - Dictioneries.py
@@ -0,0 +1,10 @@
+# Fill in the blanks to define a valid dictionary with two elements.
+cars = {"BMW":"blue", "Volvo": "red"}
+
+# What is the result of this code?
+test = { }
+print(test[0])
+# result = KeyError
+
+# Which of these values can't be used as a dictionary key?
+# answer = {2: 4, 3: 9, 4: 16,}
